18e651b9d0326cb8db75547c09c3e0a4dfd30aeb
Evans Djangbah <djangbahevans@yahoo.com>
2022-04-10 10:51:36 +0000

    extracted class generator to separate file

--- src/components/ContactDisplay.tsx
+++ src/components/ContactDisplay.tsx
@@ -2,6 +2,7 @@ import { Avatar, Box, Divider, Stack, Typography } from "@mui/material"
 import { green } from "@mui/material/colors"
 import { KeyboardArrowDown, PushPin, VolumeOff } from "@mui/icons-material"
 import { useMemo } from "react"
+import randomClassGen from "../utilities/random-class-gen"
 
 interface IContactDisplayProps {
   name: string
@@ -10,24 +11,18 @@ interface IContactDisplayProps {
   msgCount?: number
   muted?: boolean
   pinned?: boolean
+  img?: string
 }
 
 
 
-export const ContactDisplay = ({ muted, pinned, name, text, time, msgCount }: IContactDisplayProps) => {
-  const dropArrowClassName = useMemo(() => {
-    var text = "";
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for (var i = 0; i < 5; i++)
-      text += charset.charAt(Math.floor(Math.random() * charset.length));
-
-    return text;
-  }, [])
+export const ContactDisplay = ({ muted, pinned, name, text, time, msgCount, img }: IContactDisplayProps) => {
+  const dropArrowClassName = useMemo(() => randomClassGen(5), [])
 
   return (
     <>
       <Box sx={{ height: "70px", display: "flex", alignItems: "center", p: 1, cursor: "pointer", "&:hover": { bgcolor: "primary.main", [`& .arrow-down_${dropArrowClassName}`]: { transform: "translateX(0)" } } }}>
-        <Avatar alt="Evans Djangbah" sx={{ mr: 1 }} />
+        <Avatar alt="Evans Djangbah" src={img} sx={{ mr: 1 }} />
         <Stack direction="column" justifyContent="space-between">
           <Typography>{name}</Typography>
           <Typography sx={{ fontSize: "0.8rem" }}>{text}</Typography>
---
+++ src/utilities/random-class-gen.tsx
@@ -0,0 +1,8 @@
+export default (len: number) => {
+  var text = "";
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (var i = 0; i < len; i++)
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
+}
