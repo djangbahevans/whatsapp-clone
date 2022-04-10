3e8c2acbe2ff09a836a3c1d2183ad558b2f56b37
Evans Djangbah <djangbahevans@yahoo.com>
2022-04-10 10:50:03 +0000

    Added read and delivered marks to sent messages

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,4 +1,5 @@
 import { Box, Stack, Typography } from "@mui/material"
+import { Check } from "@mui/icons-material"
 
 interface IMessageProps {
   type: "from" | "to"
@@ -8,16 +9,23 @@ interface IMessageProps {
   time: string
 }
 
-const HEADER_SIZE = 64
-
-export const Message = ({ type, text }: IMessageProps) => {
+export const Message = ({ type, text, time, read, delivered }: IMessageProps) => {
   return (
     <Stack direction="row">
       {type === "from" && <Box sx={{ flexGrow: 1 }} />}
-      <Box
-        sx={{ bgcolor: type === "from" ? "#d9fdd3" : "#ffffff", maxWidth: "700px", p: 1, pr: 3, my: 1, mx: 1, borderRadius: "10px" }}>
-        <Typography>{text}</Typography>
-      </Box>
+      <Stack
+        direction="column"
+        alignItems="end"
+        sx={{ position: "relative", bgcolor: type === "from" ? "#d9fdd3" : "#ffffff", maxWidth: "700px", p: 1, pr: 3, my: "1px", mx: 1, borderRadius: "10px" }}>
+        <Typography sx={{ color: "#111b21" }}>{text}</Typography>
+        <Stack direction="row" justifyContent="center" sx={{ color: "#667781" }}>
+          <Typography sx={{ fontSize: "13px", color: "#667781" }}>{time}</Typography>
+          {type === "from" && <Box sx={{ position: "relative" }}>
+            <Check sx={{ width: "20px", height: "20px", color: read ? "#53bdeb" : "inherit" }} />
+            {delivered && <Check sx={{ position: "absolute", left: "5px", top: "0px", width: "20px", height: "20px", color: read ? "#53bdeb" : "inherit" }} />}
+          </Box>}
+        </Stack>
+      </Stack>
       {type === "to" && <Box sx={{ flexGrow: 1 }} />}
     </Stack>
   )
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -6,20 +6,22 @@ const HEADER_SIZE = 64
 
 export const Messages = () => {
   return (
-    <Stack sx={{ px: 5, height: `calc(100vh - ${HEADER_SIZE}px)`, background: `url(${BackgroundImage})`, overflowY: "scroll" }}>
+    <Stack sx={{ px: 5, height: `calc(100vh - ${HEADER_SIZE}px - 66px)`, background: `url(${BackgroundImage})`, overflowY: "scroll" }}>
       <Box sx={{ flexGrow: 1 }} />
       <Message
         type="from"
         time="9:09 PM"
-        text="If you want to understand it well, you'll have to install it on your PC." />
+        read={true}
+        delivered={true}
+        text="If you want to understand it well, you'll have to install it on your PC.1" />
       <Message
         type="to"
         time="9:09 PM"
         text="Waiting for a reply from Dr Ramde, he says he’ll check his schedule and get back to me on the possibility of having a zoom class tomorrow" />
       <Message
-        type="to"
+        type="from"
         time="9:09 PM"
-        text="Waiting for a reply from Dr Ramde, he says he’ll check his schedule and get back to me on the possibility of having a zoom class tomorrow" />
+        text="You've done well 😂" />
       <Message
         type="to"
         time="9:09 PM"
@@ -39,6 +41,7 @@ export const Messages = () => {
       <Message
         type="from"
         time="9:09 PM"
+        delivered={true}
         text="Waiting for a reply from Dr Ramde, he says he’ll check his schedule and get back to me on the possibility of having a zoom class tomorrow" />
       <Message
         type="to"
